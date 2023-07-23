260e4e49b4c464350360b21948a67e439ff4ed41
Brian <brian@vvgonline.com>
2023-07-23 17:47:38 +0100

    feat: add Species Name component

---
+++ src/components/SpeciesName.tsx
@@ -0,0 +1,28 @@
+// This component is a form input field
+// It is used to enter the species name
+
+interface SpeciesNameProps {
+  speciesName: string;
+  onChangeSpeciesName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+// This inteface defines the props expected by SpeciesName
+// onChangeSpeciesName is a function that takes an event (an input change) and returns nothing
+
+const SpeciesName: React.FC<SpeciesNameProps> = (
+  { speciesName, onChangeSpeciesName } // declares the component as a function component with the props (SpeciesNameProps)
+) => (
+  <>
+    <label htmlFor="speciesName">Species Name: </label>
+    {/*renders label "Species Name" with an htmlFor attribute which associates the label with the input
+    field */}
+    <input
+      id="speciesName"
+      type="text"
+      value={speciesName}
+      onChange={onChangeSpeciesName}
+    />{" "}
+    {/*renders input field*/}
+  </> // this renders a fragment with multiple elements (label and input)
+);
+
+export default SpeciesName;
--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
-import W12MHeader from './W12MHeader';
+import { useState } from "react";
+import W12MHeader from "./W12MHeader";
+import SpeciesName from "./SpeciesName";
 
 const W12MForm = () => {
-	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
-		</section>
-	);
+  const [speciesName, setSpeciesName] = useState<string>("humans");
+  return (
+    <section className="w12MForm">
+      <W12MHeader />
+      <SpeciesName
+        speciesName={speciesName}
+        onChangeSpeciesName={(e: any) => setSpeciesName(e.target.value)}
+      />
+    </section>
+  );
 };
 
 export default W12MForm;
