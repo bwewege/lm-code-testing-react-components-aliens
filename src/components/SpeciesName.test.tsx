3c62384667071d791943175d5215569e9e455cb1
Brian <brian@vvgonline.com>
2023-07-24 18:10:59 +0100

    feat: add test to check onChange is firing with correct params

--- src/components/SpeciesName.test.tsx
+++ src/components/SpeciesName.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import SpeciesName from "./SpeciesName";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("SpeciesName component test", () => {
   test("SpeciesName component should render", () => {
@@ -8,5 +10,25 @@ describe("SpeciesName component test", () => {
     const inputElement = screen.getByRole("textbox");
     expect(labelElement).toBeInTheDocument();
     expect(inputElement).toBeInTheDocument();
+  });
+  test("display provided value for SpeciesName", () => {
+    render(<SpeciesName speciesName="Humans" onChangeSpeciesName={() => {}} />);
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement).toHaveValue("Humans");
+  });
+  test("should call onChangeSpeciesName with correct parameters", async () => {
+    const onChangeSpeciesName = jest.fn();
+
+    render(
+      <SpeciesName speciesName="" onChangeSpeciesName={onChangeSpeciesName} />
+    );
+
+    const inputElement = screen.getByRole("textbox");
+
+    await userEvent.type(inputElement, "Mar");
+
+    expect(onChangeSpeciesName).toHaveBeenCalledTimes(3);
+
+    expect(onChangeSpeciesName).toHaveBeenLastCalledWith("r");
   });
 });
--- src/components/SpeciesName.tsx
+++ src/components/SpeciesName.tsx
@@ -3,7 +3,7 @@
 
 interface SpeciesNameProps {
   speciesName: string;
-  onChangeSpeciesName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeSpeciesName: (v: string) => void;
 }
 // This inteface defines the props expected by SpeciesName
 // onChangeSpeciesName is a function that takes an event (an input change) and returns nothing
@@ -13,13 +13,13 @@ const SpeciesName: React.FC<SpeciesNameProps> = (
 ) => (
   <>
     <label htmlFor="speciesName">Species Name: </label>
-    {/*renders label "Species Name" with an htmlFor attribute which associates the label with the input
-    field */}
+    {/*renders label "Species Name" with an htmlFor attribute 
+    which associates the label with the input field */}
     <input
       id="speciesName"
       type="text"
       value={speciesName}
-      onChange={onChangeSpeciesName}
+      onChange={(e) => onChangeSpeciesName(e.target.value)}
     />{" "}
     {/*renders input field*/}
   </> // this renders a fragment with multiple elements (label and input)
