a8d1eab5308e8b333b2169abfe771290295621b8
Brian <brian@vvgonline.com>
2023-07-25 23:18:58 +0100

    feat: add validation for NumberofBeings

--- src/components/NumberOfBeings.tsx
+++ src/components/NumberOfBeings.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
+
 interface NumberofBeingsProps {
   numberOfBeings: string;
   onChangeHandler: (v: string) => void;
@@ -6,16 +9,35 @@ interface NumberofBeingsProps {
 const NumberofBeings: React.FC<NumberofBeingsProps> = ({
   numberOfBeings,
   onChangeHandler,
-}) => (
-  <>
-    <label htmlFor="numberOfBeings">Number of Beings: </label>
-    <input
-      id="numberOfBeings"
-      type="text"
-      value={numberOfBeings}
-      onChange={(e) => onChangeHandler(e.target.value)}
-    />{" "}
-  </>
-);
+}) => {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>("");
+
+  const validate: (value: string) => string | undefined = (value) => {
+    if (
+      parseInt(value) < 1000000000 ||
+      /[a-zA-Z!@#$%^&*(),.?":{}|<>]/.test(value)
+    ) {
+      return "Enter a valid number greater than or equal to 1 billion";
+    }
+    return undefined;
+  };
+
+  return (
+    <>
+      <label htmlFor="numberOfBeings">Number of Beings: </label>
+      <input
+        id="numberOfBeings"
+        type="text"
+        value={numberOfBeings}
+        onChange={(e) => {
+          const errorMessage = validate(e.target.value);
+          setErrorMessage(errorMessage);
+          onChangeHandler(e.target.value);
+        }}
+      />
+      <ErrorMessage errorMessage={errorMessage} />
+    </>
+  );
+};
 
 export default NumberofBeings;
